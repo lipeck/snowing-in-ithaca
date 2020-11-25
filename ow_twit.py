3f65a5989b9eafd635f77bd07468fcf151ebb34f
lisa <46632998+lipeck@users.noreply.github.com>
2020-11-25 12:46:20 -0500

    ow + twitter

---
+++ ow_twit.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import tweepy
+
+with open('keys.json') as f:
+	keys = json.load(f)
+
+url = keys[0]['ow_api']
+consumer_key = keys[0]['twit_api']
+consumer_secret = keys[0]['twit_api_secret']
+access_token = keys[0]['sw_access']
+access_token_secret = keys[0]['sw_access_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+r = requests.get(url)
+
+data = r.json()
+
+weather = data['weather']
+
+for current in weather:
+    condition = current['main']
+
+if condition == 'Snow':
+	api.update_status('yes')
+else:
+	api.update_status('no')
+
+	# print(condition)
