899e38a22313ed2b5fc8e55be003f3dd3a393592
lisa <46632998+lipeck@users.noreply.github.com>
2020-11-30 23:17:42 -0500

    got it to work!

--- noaa_test.py
+++ noaa_test.py
@@ -10,7 +10,9 @@ from noaa.noaa_api_v2 import NOAAData
 
 data = NOAAData(api_token)
 
-categories = data.data_categories(locationid='FIPS:37', sortfield='name')
+weather_data = data.fetch_data(datasetid='GHCND', locationid='ZIP:14850', startdate='2020-01-01', enddate='2020-01-02')
 
-for i in categories:
-    print(i)
+# for i in categories:
+#     print(i)
+
+print(json.dumps(weather_data, indent = 4, sort_keys = True))
