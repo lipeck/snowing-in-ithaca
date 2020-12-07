b6977ce0f859785fb6e12a03e20ebfb6bde5e0ad
lisa <46632998+lipeck@users.noreply.github.com>
2020-12-06 20:51:25 -0500

    update while loop

--- noaa_loop_test.py
+++ noaa_loop_test.py
@@ -2,6 +2,7 @@ import requests
 import json
 import datetime
 from dateutil.relativedelta import relativedelta
+import time
 
 with open('keys.json') as f:
 	keys = json.load(f)
@@ -11,7 +12,7 @@ api_token = keys[0]['noaa_api']
 from noaa.noaa_api_v2 import NOAAData
 data = NOAAData(api_token)
 
-test_date = '2013-12-20'
+test_date = '2020-02-24'
 
 #subtracts one year from date
 date_clean = datetime.datetime.strptime(test_date, '%Y-%m-%d').date()
@@ -31,15 +32,13 @@ noaa_date = weather_data[0]['date']
 x = 0
 
 while float(snowfall) == 0:
-
-	if float(snowfall) > 0:
-		break
-		print('it snowed ' + snowfall + ' ' + noaa_date)
-	if float(snowfall) == 0:
 		x = x+1
-		print(x)
+		# print(x)
 		back = last_year - relativedelta(years=int(x))
 		weather_data = data.fetch_data(stationid='GHCND:USC00304174', datasetid='GHCND', startdate=back, enddate=back, datatypeid='SNOW', units='standard')
 		snowfall = weather_data[0]['value']
 		noaa_date = weather_data[0]['date']
-		print('it didn`t ' + str(snowfall) + ' ' + noaa_date)
+		time.sleep(1)
+		if float(snowfall) > 0:
+			print('it snowed ' + str(snowfall) + ' ' + noaa_date)
+			break
