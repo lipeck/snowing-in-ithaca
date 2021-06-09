c9b0fec9ef8d7a337d1b7f06a25ba0e244532039
lisa <46632998+lipeck@users.noreply.github.com>
2021-06-09 00:04:28 -0400

    worked out some date search quirks

--- test-and-setup/snowfall_test.py
+++ test-and-setup/snowfall_test.py
@@ -1,5 +1,7 @@
 import csv
 
+last_year_day = "-02-01"
+
 with open('../snowfall.csv', 'r') as f:
     reader = csv.DictReader(f)
     
@@ -8,10 +10,16 @@ with open('../snowfall.csv', 'r') as f:
 
     for row in reader:
         date = row['DATE']
+        #convert snowfall values to integers
         snow = int(float(row['SNOW']))
 
         dates.append(date)
         snowamt.append(snow)
 
-        if snow > 16:
+#prints snowfall level
+        # if snow > 16:
+        #     print(row['DATE'], row['SNOW'])
+
+        # prints years with snowfall data
+        if last_year_day in date and snow >= 0.5:
             print(row['DATE'], row['SNOW'])
